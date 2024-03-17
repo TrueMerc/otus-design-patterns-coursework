dbbc1046fa115318b03cbdf48c2b0d7f51673a5b
Vladimir Ryabtsev <true_merc@mail.ru>
2024-03-17 21:31:54 +0300

    Fix Spring based tests

--- src/test/java/ru/ryabtsev/antifraud/TestDemoApplication.java
+++ src/test/java/ru/ryabtsev/antifraud/TestDemoApplication.java
@@ -1,23 +1,15 @@
-//package ru.ryabtsev.antifraud;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-//import org.springframework.context.annotation.Bean;
-//import org.testcontainers.containers.PostgreSQLContainer;
-//import org.testcontainers.utility.DockerImageName;
-//
-//@TestConfiguration(proxyBeanMethods = false)
-//public class TestDemoApplication {
-//
-//	@Bean
-//	@ServiceConnection
-//	PostgreSQLContainer<?> postgresContainer() {
-//		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-//	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication.from(Application::main).with(TestDemoApplication.class).run(args);
-//	}
-//
-//}
+package ru.ryabtsev.antifraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(initializers = DatabaseContextInitializer.class)
+public class TestDemoApplication extends DatabaseContextInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestDemoApplication.class).run(args);
+	}
+
+}
